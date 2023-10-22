3d5ec79e1794de1dc64b2c771883d5beba524c34
Laputa <nguyencong160602@gmail.com>
2023-10-23 01:19:14 +0700

    update

--- app/src/main/java/com/example/order_food/adminFragment/AccountManagementFragment.java
+++ app/src/main/java/com/example/order_food/adminFragment/AccountManagementFragment.java
@@ -2,7 +2,9 @@ package com.example.order_food.adminFragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,33 +19,13 @@ import com.example.order_food.R;
  */
 public class AccountManagementFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public AccountManagementFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountManagementFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountManagementFragment newInstance(String param1, String param2) {
+    public static AccountManagementFragment newInstance() {
         AccountManagementFragment fragment = new AccountManagementFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +33,37 @@ public class AccountManagementFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_management, container, false);
+        View view = inflater.inflate(R.layout.fragment_account_management, container, false);
+
+        AppCompatButton buttonCreate = view.findViewById(R.id.button_create_account);
+        AppCompatButton buttonView = view.findViewById(R.id.button_view_account);
+
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with CreateFoodFragment
+                replaceFragment(new CreateAccoutFragment());
+            }
+        });
+
+        buttonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new ViewListAccoutsFragment());
+            }
+        });
+
+        return view;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainerView2, fragment);
+        transaction.commit();
     }
 }
